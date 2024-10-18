708d964eeda5e6905e56beaab1aa4b718a399f02
umetsumomoka <t236143@st.yamagata-u.ac.jp>
2024-10-18 19:30:56 +0900

    Add files via upload

---
+++ 足し算課題.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int k;
+    printf("How many chocolate");
+    scanf("%d",&k);
+    
+    printf("The nuber of chocolate is %d",k);
+    
+    int j;
+    printf("How many candy");
+    scanf("%d",&j);
+    
+    printf("The unber of candy is %d",j);
+    
+    printf("allsweet is %d",k+j);
+    
+
+    return 0;
+}
